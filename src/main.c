8c08fa72f7b9c3cdc53fd060638ead7285e002e3
rmsdud <rmsdud@github.com>
2017-11-11 20:40:43 +0900

    signal handling updated

--- src/main.c
+++ src/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <signal.h>
 #include "commands.h"
 #include "built_in.h"
 #include "utils.h"
+#include "signal_handlers.h"
 
 int main()
 {
   char buf[8096];
-
+  signal(SIGINT,catch_sigint);
+  signal(SIGTSTP,catch_sigtstp);
   while (1) {
     fgets(buf, 8096, stdin);
-
     struct single_command commands[512];
     int n_commands = 0;
     mysh_parse_command(buf, &n_commands, &commands);
--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,15 @@
 #include "signal_handlers.h"
+#include <stdio.h>
+#include <signal.h>
 
 void catch_sigint(int signalNo)
 {
-  // TODO: File this!
+  if(signalNo==SIGINT)
+	printf("\nYour input Ctrl+C\nShell doesn't close.\n");
 }
 
 void catch_sigtstp(int signalNo)
 {
-  // TODO: File this!
+  if(signalNo==SIGTSTP)
+	printf("\nYour input Ctrl+Z, but it doesn't move.\n");
 }
